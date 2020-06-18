9142d0fbefeb07fe4937b061652812ba43765df1
moskin <oleksiimoskin@Moskins-MacBook-Pro.local>
2020-06-18 20:46:17 +0300

    Updates

---
+++ precache-manifest.0dc93aeca556db0c48a4c67f1c048798.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aca18080ee424a061852c4bbf7510e6",
+    "url": "/my-social-network/index.html"
+  },
+  {
+    "revision": "3c8bbcd4f773d4ce63a4",
+    "url": "/my-social-network/static/css/3.f6fdc03a.chunk.css"
+  },
+  {
+    "revision": "19fbef8081f2a6803dfc",
+    "url": "/my-social-network/static/css/4.13108643.chunk.css"
+  },
+  {
+    "revision": "98d69702b3b59f226eb1",
+    "url": "/my-social-network/static/css/main.3cc436f6.chunk.css"
+  },
+  {
+    "revision": "5c042af4fb0bbe8afe8f",
+    "url": "/my-social-network/static/js/2.d474f908.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-social-network/static/js/2.d474f908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8bbcd4f773d4ce63a4",
+    "url": "/my-social-network/static/js/3.7156bc43.chunk.js"
+  },
+  {
+    "revision": "19fbef8081f2a6803dfc",
+    "url": "/my-social-network/static/js/4.39849d3d.chunk.js"
+  },
+  {
+    "revision": "98d69702b3b59f226eb1",
+    "url": "/my-social-network/static/js/main.bdd5d081.chunk.js"
+  },
+  {
+    "revision": "46ca13ede67476eb927e",
+    "url": "/my-social-network/static/js/runtime-main.4b096d2d.js"
+  },
+  {
+    "revision": "78c748626b335bcd4353c2036d650535",
+    "url": "/my-social-network/static/media/pacpman-loader.78c74862.svg"
+  }
+]);
